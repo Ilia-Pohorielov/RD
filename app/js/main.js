901b96f277b11562609659cbf622e648847e3f50
ilia.pohorielov <ilia.pohorielov@gmail.com>
2018-02-03 18:30:23 +0200

    homepage, TODO : one block and mobile

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
     $('select').material_select();
+    function heightEL(el) {
+        var elH = el;
+        var maxHeight = 0;
+        for (var i = 0; i < elH.length; ++i) {
+            elH[i].style.height = "";
+            if (maxHeight < elH[i].clientHeight) {
+                maxHeight = elH[i].clientHeight;
+            }
+        }
+        for (var i = 0; i < elH.length; ++i) {
+            elH[i].style.height = maxHeight + "px";
+        }
+    }
+    heightEL($('.news-item__info .title-news'));
+    heightEL($('.news-item__info .text'));
+
+    $('.js-slider-position').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    });
 });
